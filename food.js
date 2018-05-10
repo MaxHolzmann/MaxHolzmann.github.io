682028e29de5dd7d56fd3aac89f090efbdd30b0f
Max Holzmann <max@holzmann.io>
2018-05-10 15:05:00 +0000

    More food places

--- food.js
+++ food.js
@@ -7,13 +7,20 @@ function getRandomInt(min, max) {
 
 //lists of places
 var all = ["McDonalds", "Culvers", "Lil Ceasers", "Taco Bell", "Taco Johns", "Pizza Ranch", "Jet's Pizza",
-"Burger King", "Dunkin' Donuts", "Kentucky Fried Chicken", "Fazoli's", "Backyard Bar and Grill"];
+"Burger King", "Dunkin' Donuts", "Kentucky Fried Chicken", "Fazoli's", "Backyard Bar and Grill", "Sal's Pizza", "Bob's Pizza", "Panda Express",
+"A&W", "Noodles and Company", "Rocky Rococos", "Papa Johns", "Urban Fuel", "Pamela's Bar & Grill", "Five Guys (Oshkosh)", "Buffalo Wild Wings",
+"Starbucks", "Dairy Queen", "Qdoba", "Arby's", "Jimmy Johns", "Tuckers", "Subway", "Cousin Subs", "Schmittys", "11:11 Burger's & Beignets", "Gilles",
+"Panera Bread", "Schreiners", "Ala Roma", "Red Cabin"];
 
-var pizzaplaces = ["Lil Ceasers", "Pizza Ranch", "Jet's Pizza", "Dominoes"];
+var pizzaplaces = ["Lil Ceasers", "Pizza Ranch", "Jet's Pizza", "Dominoes", "Bob's Pizza", "Sal's Pizza", 
+"Rocky Rococos", "Papa Johns"];
 
-var fastfoods = ["McDonalds", "Culvers", "Taco Bell", "Taco Johns", "Burger King", "Kentucky Fried Chicken", "Panda Express", "Hardees"];
+var fastfoods = ["McDonalds", "Culvers", "Taco Bell", "Taco Johns", "Burger King", "Kentucky Fried Chicken", "Panda Express", "Hardees", "A&W",
+"Noodles and Company", "Five Guys (Oshkosh)", "Buffalo Wild Wings", "Qdoba", "Dairy Queen", "Arby's", "Jimmy Johns", "Tuckers", "Subway", 
+"Cousin Subs", "Fazoli's", "11:11 Burger's & Beignets", "Gilles", "Panera Bread"];
 
-var restur = ["Backyard Bar & Grill", "Fat Joes", "Applebees"];
+var restur = ["Backyard Bar & Grill", "Fat Joes", "Applebees", "Urban Fuel", "Pamela's Bar & Grill", "Schmittys", "Schreiners", "Ala Roma",
+"Red Cabin"];
 
 
 function any() {
