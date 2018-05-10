db771a03596e17d9c14bca11d2c83a554a1ca064
Max Holzmann <max@holzmann.io>
2018-05-10 15:09:40 +0000

    more food places

--- food.js
+++ food.js
@@ -10,17 +10,17 @@ var all = ["McDonalds", "Culvers", "Lil Ceasers", "Taco Bell", "Taco Johns", "Pi
 "Burger King", "Dunkin' Donuts", "Kentucky Fried Chicken", "Fazoli's", "Backyard Bar and Grill", "Sal's Pizza", "Bob's Pizza", "Panda Express",
 "A&W", "Noodles and Company", "Rocky Rococos", "Papa Johns", "Urban Fuel", "Pamela's Bar & Grill", "Five Guys (Oshkosh)", "Buffalo Wild Wings",
 "Starbucks", "Dairy Queen", "Qdoba", "Arby's", "Jimmy Johns", "Tuckers", "Subway", "Cousin Subs", "Schmittys", "11:11 Burger's & Beignets", "Gilles",
-"Panera Bread", "Schreiners", "Ala Roma", "Red Cabin"];
+"Panera Bread", "Schreiners", "Ala Roma", "Red Cabin", "Joe's Fox Hut", "Mancino's", "Marco's Pizza", "Papa Murphy's"];
 
 var pizzaplaces = ["Lil Ceasers", "Pizza Ranch", "Jet's Pizza", "Dominoes", "Bob's Pizza", "Sal's Pizza", 
-"Rocky Rococos", "Papa Johns"];
+"Rocky Rococos", "Papa Johns", "Joes Fox Hut", "Marco's Pizza", "Papa Murphy's"];
 
 var fastfoods = ["McDonalds", "Culvers", "Taco Bell", "Taco Johns", "Burger King", "Kentucky Fried Chicken", "Panda Express", "Hardees", "A&W",
 "Noodles and Company", "Five Guys (Oshkosh)", "Buffalo Wild Wings", "Qdoba", "Dairy Queen", "Arby's", "Jimmy Johns", "Tuckers", "Subway", 
 "Cousin Subs", "Fazoli's", "11:11 Burger's & Beignets", "Gilles", "Panera Bread"];
 
 var restur = ["Backyard Bar & Grill", "Fat Joes", "Applebees", "Urban Fuel", "Pamela's Bar & Grill", "Schmittys", "Schreiners", "Ala Roma",
-"Red Cabin"];
+"Red Cabin", "Mancino's"];
 
 
 function any() {
