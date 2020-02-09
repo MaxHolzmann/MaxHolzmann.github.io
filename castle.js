eaf0f881c27301d65d21d8f929737388e15ce3c1
Maximilian <maximilian@maximilians-air.attlocal.net>
2020-02-09 17:52:35 -0600

    Hey man, more JSON!

--- castle.js
+++ castle.js
@@ -1,11 +1,12 @@
 const request = require('request')
-const url = 'https://maxholzmann.github.io/castle.json'
 
-const getCharacterData = (name, callback) => { 
+
+const getCharacterData = (input, callback) => { 
+    const url = 'https://maxholzmann.github.io/castleJSON/' + input + '.json'
     request({url:url, json: true}, (error, response) => {
-        const data = response.body.red 
-        console.log(data.name)
+        const data = response.body
+        console.log(data)
 })
 }
-getCharacterData('red')
+getCharacterData('castle')
     
